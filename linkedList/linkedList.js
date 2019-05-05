70cc8e739f75edece6f14ec04455ffd5001ae429
LaoLeo <1074699327@qq.com>
2019-05-06 07:16:01 +0800

    完善链表功能

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -10,19 +10,95 @@ class Node {
  */
 class SinglyLinkedList {
     constructor() {
-        this.head = new Node('head')
+        this.head = null
+        this.length = 0
     }
 
     findByValue(element) {
         let currentNode = this.head
-        while(currentNode.element !== element && currentNode.next !== null) {
+        while (currentNode.element !== element && currentNode.next !== null) {
             currentNode = currentNode.next
         }
 
         return currentNode
     }
 
-    insert() {
+    append(node) {
+        let currentNode
 
+        if (this.head === null) {
+            this.head = node
+        } else {
+            currentNode = this.head
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next
+            }
+            currentNode.next = node
+        }
+
+        return ++this.length
+    }
+
+    insert(position, node) {
+        let currentNode = this.head,
+            i = 0
+
+        if (position < 0 || position > this.length) return false
+
+        // 首尾边界情况
+        if (position === 0) {
+            node.next = this.head.next
+            this.head = node
+            this.length++
+
+                return true
+        }
+        if (position === this.length) {
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next
+            }
+            currentNode.next = node
+            this.length++
+
+                return true
+        }
+
+        while (i < position - 1) {
+            currentNode = currentNode.next
+            i++
+        }
+        node.next = currentNode.next
+        currentNode.next = node
+        this.length++
+
+            return true
+    }
+
+    /**
+     * 
+     * @param {number} position 
+     * 
+     * @return {Node || null} 返回移除的节点
+     */
+    removeAt(position) {
+        let currentNode = this.head,
+            i = 0,
+            previous
+
+        if (position < 0 || position > this.length - 1) return null
+
+        if (position === 0) {
+            this.head = this.head.next
+        } else {
+            while (i++ < position) {
+                previous = currentNode
+                currentNode = currentNode.next
+            }
+            previous.next = currentNode.next
+        }
+
+        this.length--
+
+            return currentNode
     }
 }
