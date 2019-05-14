7f5098a7e0177ed7d6bee97657bd0d7c99b18a92
laotuzhu <laotuzhu@aobi.com>
2019-05-14 17:15:10 +0800

    :sparkles: 双链表

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -5,13 +5,34 @@ class Node {
     }
 }
 
-/**
- * 单链表类
- */
-class SinglyLinkedList {
+class DoubleLinkedNode extends Node {
+    constructor(element) {
+        super(element)
+        this.pre = null
+    }
+}
+
+class LinkedList {
     constructor() {
         this.head = null
         this.length = 0
+    }
+
+    display() {
+        let currentNode = this.head
+        while(currentNode !== null) {
+            console.log(currentNode.element)
+            currentNode = currentNode.next
+        }
+    }
+}
+
+/**
+ * 单链表类 无头链表（不带哨兵节点）
+ */
+class SinglyLinkedList extends LinkedList {
+    constructor() {
+        super()
     }
 
     /**
@@ -36,6 +57,19 @@ class SinglyLinkedList {
         while (currentNode.element !== element && currentNode.next !== null) {
             currentNode = currentNode.next
         }
+
+        return currentNode
+    }
+
+    findPrev(element) {
+        let currentNode = this.head
+        if (currentNode === null) return -1
+
+        while (currentNode.next !== null && currentNode.next.element !== element) {
+            currentNode = currentNode.next
+        }
+
+        if (currentNode.next === null) return -1
 
         return currentNode
     }
@@ -91,6 +125,19 @@ class SinglyLinkedList {
             return true
     }
 
+    remove(element) {
+        const node = this.findByValue(element)
+        if (node === -1) return false
+
+        // 无头链表要处理边界情况
+        if (this.length === 1) {
+            this.head = null
+            return true
+        }
+        let prevNode = this.findPrev(element)
+        prevNode.next = node.next
+    }
+
     /**
      * 
      * @param {number} position 
@@ -120,7 +167,75 @@ class SinglyLinkedList {
     }
 }
 
-// TODO: 双链表
+// TODO: 双链表 带头链表
+class DoubleLinkedList extends LinkedList {
+    constructor() {
+        super()
+        this.head = new DoubleLinkedNode('HEAD')
+    }
+
+    append(element) {
+        const node = new DoubleLinkedNode(element)
+        let currentNode = this.head
+
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next
+        }
+        currentNode.next = node
+        node.pre = currentNode
+
+        return ++this.length
+    }
+
+    findByValue(element) {
+        let currentNode = this.head
+        while (currentNode !== null && currentNode.element !== element) {
+            currentNode = currentNode.next
+        }
+
+        return currentNode !== null ? currentNode : -1
+    }
+
+    /**
+     * 指定元素向后插入
+     * 
+     * @param {*} newElement 
+     * @param {*} element 
+     */
+    insert(newElement, element) {
+        const node = this.findByValue(element)
+        if (node === -1) return false
+
+        const newNode = new DoubleLinkedNode(newElement)
+        newNode.next = node.next
+        node.next = newNode
+        newNode.pre = node
+        if (newNode.next !== null) {
+            newNode.next.pre = newNode
+        }
+
+        this.length++
+
+        return true
+    }
+
+    remove(element) {
+        const node = this.findByValue(element)
+        if (node === -1) return false
+
+        node.pre.next = node.next
+        if (node.next !== null) {
+            node.next.pre = node.pre
+        }
+
+        node.next = null
+        node.pre = null
+        
+        this.length--
+
+        return true
+    }
+}
 
 // TODO: 循环单链表
 
@@ -128,5 +243,7 @@ class SinglyLinkedList {
 
 module.exports = {
     SinglyLinkedList,
-    Node
+    DoubleLinkedList,
+    Node,
+    DoubleLinkedNode
 }
--- linkedList/playground.js
+++ linkedList/playground.js
@@ -1,4 +1,4 @@
-const { SinglyLinkedList } = require('./linkedList')
+const { SinglyLinkedList, DoubleLinkedList } = require('./linkedList')
 const { isPalindrome } = require('./algorithm')
 
 // isPalidrome
@@ -9,3 +9,17 @@ const link = SinglyLinkedList.create(str.split(''))
 const link_strnot = SinglyLinkedList.create(str_not.split(''))
 console.debug(str + ' isPalindrome:', isPalindrome(link))
 console.debug(str_not + ' isPalindrome:', isPalindrome(link_strnot))
+
+console.log("========doubleLinkedList=========")
+const doubleLink = new DoubleLinkedList()
+doubleLink.append('a')
+doubleLink.append('b')
+doubleLink.append('c')
+doubleLink.display()
+doubleLink.insert('d', 'a')
+doubleLink.insert('e', 'c')
+doubleLink.display()
+doubleLink.remove('a')
+doubleLink.remove('b')
+doubleLink.remove('e')
+doubleLink.display()
