510cb55f5bc07887db11e2d03eae41a42e374840
LaoLeo <1074699327@qq.com>
2019-05-22 23:07:57 +0800

    stack

---
+++ stack/algo.js
@@ -0,0 +1,43 @@
+const { StackBaseOnLinklist } = require('./stack')
+
+/**
+ * 简单的浏览器前进后退功能
+ */
+class SimpleBlowser {
+    constructor() {
+        this.normalStack = new StackBaseOnLinklist()
+        this.backStack = new StackBaseOnLinklist()
+    }
+
+    // 正常浏览页面
+    push(url) {
+        this.normalStack.push(url)
+        this.backStack.clear()
+        this.normalStack.display()
+    }
+
+    // 后退
+    back() {
+        const url = this.normalStack.pop()
+        this.backStack.push(url)
+        this.normalStack.display()
+    }
+
+    // 前进
+    front() {
+        this.backStack.display()
+        const url = this.backStack.pop()
+        if (!url) console.log('无法前进')
+        else this.normalStack.push(url)
+
+        this.normalStack.display()
+    }
+}
+
+const blowser = new SimpleBlowser()
+blowser.push('baidu.com')
+blowser.push('mail.qq.com')
+blowser.back()
+blowser.front()
+blowser.back()
+blowser.push('taobao.com')
